d5ba978d0c2e208a865681588b7cca1e5486774b
Pedro Henrique <pedroh658@gmail.com>
2025-12-09 19:07:36 -0300

    first commit

---
+++ clinica medica/js/jpmedic.js
@@ -0,0 +1,14 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    const nome = frm.inNome.value
+    const nasci = Number(frm.inNasc.value)
+    const telefone = Number(frm.inTel.value)
+    resp1.innerText = `Recebemos o seu contato em breve vc será atendido por um de nossos agentes.`
+})
+
+frm.addEventListener("reset", (e) =>{
+    resp1.innerHTML = ``
+})
