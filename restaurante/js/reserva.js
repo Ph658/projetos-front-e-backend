051d77362b83274245f5d0c9dc35cf203a0c96c5
Pedro <pedroh658@gmail.com>
2026-05-27 07:55:17 -0300

    First Commit

--- restaurante/js/reserva.js
+++ restaurante/js/reserva.js
@@ -1,6 +1,7 @@
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("#resp")
 const resp2 = document.querySelector("#resp2")
+const resp3 = document.querySelector("#resp3")
 
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
@@ -10,17 +11,20 @@ frm.addEventListener("submit", (e) =>{
     const pagamento = frm.inDebito.checked
     
     if(pagamento)
-        resp2.innerText = `Forma de pagamento selecionado: débito.`
+        resp2.innerText = `Forma de pagamento selecionado: Débito.`
 
     else{
-        resp2.innerText =`Forma de pagamento selecionado: crédito`
+        resp2.innerText =`Forma de pagamento selecionado: Crédito`
     }
-
-    resp1.innerText = `Nome: ${nome} \n Endereço: ${end}\n Tel: ${tel}`
+    
+    resp1.innerText = `Pedido Confirmado!`
+    resp3.innerText = `Nome: ${nome} \n Endereço: ${end}\n Tel: ${tel}`
+    
 })
 
 
 frm.addEventListener("reset",() =>{
     resp1.innerText=""
     resp2.innerText=""
+    resp3.innerText=""
 })
